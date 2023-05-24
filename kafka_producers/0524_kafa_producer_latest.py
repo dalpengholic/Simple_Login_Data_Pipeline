6459dde8a108ee29f43c9198bab90bf038a8f787
dalpengholic <dalpengholic@gmail.com>
2023-05-24 15:54:12 +0300

    Overhaul kafka-producer related stuff.

---
+++ kafka_producers/0524_kafa_producer_latest.py
@@ -0,0 +1,60 @@
+from confluent_kafka import Producer
+from confluent_kafka.serialization import StringSerializer, SerializationContext, MessageField
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
+from confluent_kafka.serialization import SerializationContext
+from login_events import create_login_event
+
+
+
+
+# Define the Kafka topic
+topic = 'mock_login_topic'
+# Define the Kafka bootstrap servers and Schema Registry URL
+bootstrap_servers = 'broker:9092'
+schema_registry_url = 'http://schema-registry:8081'
+# Create a Schema Registry client
+schema_registry_conf = {'url': schema_registry_url}
+schema_registry_client = SchemaRegistryClient(schema_registry_conf)
+# Define the Avro schema for the login event as a string
+avro_schema_str = '''
+{
+    "type": "record",
+    "name": "LoginEvent",
+    "fields": [
+        {"name": "user_id", "type": "int"},
+        {"name": "timestamp", "type": "string"},
+        {"name": "browser_info", "type": "string"}
+    ]
+}
+'''
+
+# Create an Avro serializer with the Schema Registry client and the Avro schema
+avro_serializer = AvroSerializer(schema_str=avro_schema_str, schema_registry_client=schema_registry_client)
+
+# Create a SerializationContext
+serialization_context = SerializationContext(
+        topic=topic,
+        field=MessageField.VALUE  # Specify the field being serialized (in this case, the message value)
+    )
+
+
+# Create a Kafka producer instance
+producer_conf = {
+    'bootstrap.servers': bootstrap_servers
+#    'key.serializer': StringSerializer('utf_8'),
+#    'value.serializer': avro_serializer
+}
+producer = Producer(producer_conf)
+
+login_event = create_login_event()
+serialized_login_event = avro_serializer(login_event, serialization_context) 
+
+def acked(err, msg):
+    if err is not None:
+        print("Failed to deliver message: %s: %s" % (str(msg), str(err)))
+    else:
+        print("Message produced: %s" % (str(msg)))
+# Produce the login event
+producer.produce(topic=topic, key=None, value=serialized_login_event, callback=acked)
+producer.poll(1)
---
+++ kafka_producers/login_events.py
@@ -0,0 +1,24 @@
+import random
+from datetime import datetime, timedelta
+
+def create_login_event():
+    browsers = ['safari', 'chrome', 'firefox', 'edge']
+    
+    # Generate a random timestamp within the range of 2022 Jan 1st and 2022 Dec 31st
+    user_id = random.randint(1, 500)
+    start_date = datetime(2022, 1, 1)
+    end_date = datetime(2022, 12, 31)
+    random_date = start_date + timedelta(seconds=random.randint(0, int((end_date - start_date).total_seconds())))
+    timestamp = random_date.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    # Select a random browser from the list
+    browser = random.choice(browsers)
+
+    # Create the login event record
+    login_event = {
+        'user_id': user_id,
+        'timestamp': timestamp,
+        'browser_info': browser
+    }
+
+    return login_event
